4af141c424f947897a94ccdaccc0e73cc58f95ef
gitstep1 <leeannbianca315@gmail.com>
2022-09-07 16:28:24 +0200

    first commit

---
+++ components/Login.js
@@ -0,0 +1,22 @@
+import {Text, View, StyleSheet, Button, TextInput} from 'react-native';
+
+
+function Login(){
+  return(
+    <View>
+     <TextInput
+     placeholder="Enter Password"
+     
+     /><br></br>
+
+     <TextInput
+     placeholder="Enter UserName"
+     
+     />
+
+      </View>
+
+  )
+}
+
+export default Login;
